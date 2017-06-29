d6d3d641e105860df7e01988f4e260c96c95b3b3
kortes <sergey@4-7i.ru>
2017-06-29 12:12:24 +0500

    mobile home & calc

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+	var mobile = $(window).width() < 768;
+
 	if ($(".select").length > 0) {
 		$(".select").select2({
 		    width: 'auto'
@@ -10,6 +12,39 @@ $(document).ready(function() {
 		$(".select").on("select2:close", function() {
 		    $(".select2-search__field").attr("placeholder", null);
 		});
+	}
+
+	if (mobile) {
+		if ($('.recently__list').length > 0) {
+			$('.recently__list').slick({
+				dots: true,
+				infinite: true,
+				arrows: false,
+				responsive: [
+					{
+				      breakpoint: 768,
+				      settings: {
+				        slidesToShow: 2,
+				        slidesToScroll: 2
+				      }
+				    },
+				    {
+				      breakpoint: 330,
+				      settings: {
+				        slidesToShow: 1,
+				        slidesToScroll: 1
+				      }
+				    }
+				]
+			});
+		}
+
+		$(".menu-burger").click(function(e){
+			e.preventDefault();
+			var self = $(this);
+			var parent = self.closest(".header");
+			parent.toggleClass("header_menu_active");
+		})
 	}
 	
 });

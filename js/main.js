cc359f363ad54531668e4fe6c68930f275c8dd34
kortes <sergey@4-7i.ru>
2017-07-23 14:30:27 +0500

    fix bugs & add autocomplite

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 $(document).ready(function() {
 	var mobile = $(window).width() < 768;
 
-	if ($(".select").length > 0) {
-		$(".select").select2({
-		    width: 'auto'
-		});
+	// if ($(".select").length > 0) {
+	// 	$(".select").select2({
+	// 	    width: 'auto'
+	// 	});
 
-		$(".select").on("select2:open", function() {
-		    $(".select2-search__field").attr("placeholder", "Город");
-		});
-		$(".select").on("select2:close", function() {
-		    $(".select2-search__field").attr("placeholder", null);
-		});
+	// 	$(".select").on("select2:open", function() {
+	// 	    $(".select2-search__field").attr("placeholder", "Город");
+	// 	});
+	// 	$(".select").on("select2:close", function() {
+	// 	    $(".select2-search__field").attr("placeholder", null);
+	// 	});
+	// }
+
+	if ($(".inp_type_city").length > 0) {
+		var city = [
+			"Архангельск",
+			"Екатеринбург",
+			"Москва",
+			"Салехард",
+			"Сыктывкар",
+			"Ханты-Мансий"
+		]
+
+		$(".inp_type_city").autocomplete({
+	    	source: city,
+	    	minLength: 0,
+	    	open: function( event, ui ) {
+	    		$(this).addClass("inp_type_city_open");
+	    	},
+	    	close: function( event, ui ) {
+	    		$(this).removeClass("inp_type_city_open");
+	    	}
+	    });
 	}
 
 	if (mobile) {
